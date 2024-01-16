2ed0e5e90a1de21409a5e07b28288c61498cceb7
OM-TIWARI123 <90179771+OM-TIWARI123@users.noreply.github.com>
2024-01-16 12:09:29 +0530

    error solved

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const cors=require('cors');
 
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
  app.listen(3000,()=>{
     console.log("Server is running on port 3000");
  })
